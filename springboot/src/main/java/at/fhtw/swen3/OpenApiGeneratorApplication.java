1b95ca4f60cdfe4d2938aac3650239c4e681840e
Carmen Kratzer <if20b111@technikum-wien.at>
2023-02-18 18:06:09 +0100

    first commit

---
+++ springboot/src/main/java/at/fhtw/swen3/OpenApiGeneratorApplication.java
@@ -0,0 +1,23 @@
+package at.fhtw.swen3;
+
+import com.fasterxml.jackson.databind.Module;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
+
+@SpringBootApplication
+public class OpenApiGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiGeneratorApplication.class, args);
+    }
+
+    @Bean(name = "at.fhtw.swen3.OpenApiGeneratorApplication.jsonNullableModule")
+    public Module jsonNullableModule() {
+        return new JsonNullableModule();
+    }
+
+}
