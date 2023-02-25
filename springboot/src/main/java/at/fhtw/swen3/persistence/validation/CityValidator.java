e330f92e93411e763fefb002b6f597b35cea54d8
Carmen Kratzer <if20b111@technikum-wien.at>
2023-02-25 11:31:27 +0100

    implementation of BL Validation

---
+++ springboot/src/main/java/at/fhtw/swen3/persistence/validation/CityValidator.java
@@ -0,0 +1,24 @@
+package at.fhtw.swen3.persistence.validation;
+
+import at.fhtw.swen3.persistence.entities.RecipientEntity;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
+
+public class CityValidator implements ConstraintValidator<City, RecipientEntity> {
+
+    private static final Pattern CITY_PATTERN = Pattern.compile("^[A-Z][ a-zA-Z-/]*");
+
+    @Override
+    public void initialize (City constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+    public boolean isValid(RecipientEntity recipient, ConstraintValidatorContext constraintValidatorContext) {
+        if("Österreich".equals(recipient.getCountry()) || "Austria".equals(recipient.getCountry())) {
+            return CITY_PATTERN.matcher(recipient.getCity()).matches();
+        } else {
+            return true;
+        }
+    }
+}
---
+++ springboot/src/main/java/at/fhtw/swen3/persistence/validation/Street.java
@@ -0,0 +1,18 @@
+package at.fhtw.swen3.persistence.validation;
+
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = StreetValidator.class)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Street {
+    String message() default "Street does not match pattern.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
